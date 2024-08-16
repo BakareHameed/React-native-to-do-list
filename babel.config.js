30b0ee4e53ed5bbc664461358596fc4cf802a61f
BakareHameed <bakrehameed07@gmall.com>
2024-08-16 11:12:57 +0100

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    // plugins: ["expo-router/babel"],
+  };
+};
+
+// 3000300166
+// 3000600166
